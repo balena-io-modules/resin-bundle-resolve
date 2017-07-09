bc74e16d6646af30b72ec8559dd0cb466082c96e
Pagan Gazzard <page@resin.io>
2017-07-08 21:16:13 -0700

    Make install scripts and wscript/.gyp installs that require the source to be available work by using a different dockerfile for them
    Change-type: minor

--- src/resolvers/nodeResolver.ts
+++ src/resolvers/nodeResolver.ts
@@ -1,21 +1,26 @@
 import * as Promise from 'bluebird'
 
+import * as _ from 'lodash'
+
 import { Bundle, FileInfo, Resolver } from '../resolver'
 
 export default class NodeResolver implements Resolver {
 	public priority = 0
 	public name = 'NodeJS'
 
-	private hasPackageJson = false
+	private packageJsonContent?: Buffer
+	private hasScripts = false
 
 	public entry(file: FileInfo): void {
 		if (file.name == 'package.json') {
-			this.hasPackageJson = true
+			this.packageJsonContent = file.contents
+		} else if (file.name === 'wscript' || _.endsWith(file.name, '.gyp')) {
+			this.hasScripts = true
 		}
 	}
 
 	public isSatisfied(bundle: Bundle): boolean {
-		return this.hasPackageJson
+		return this.packageJsonContent != null
 	}
 
 	public resolve(bundle: Bundle): Promise<FileInfo[]> {
@@ -23,24 +28,46 @@ export default class NodeResolver implements Resolver {
 		// Use latest node base image. Don't use the slim image just in case
 		// TODO: Find out which apt-get packages are installed mostly with node
 		// base images.
-		const dockerfile = `
-			FROM resin/${bundle.deviceType}-node
-			WORKDIR /usr/src/app
-			RUN ln -s /usr/src/app /app
+		return Promise.try(() =>
+			JSON.parse(this.packageJsonContent!.toString())
+		).catch((e: Error) => {
+			throw new Error(`package.json: ${e.message}`)
+		}).then((packageJson) => {
+			if (!_.isObject(packageJson)) {
+				throw new Error('package.json: must be a JSON object')
+			}
 
-			COPY package.json .
-			RUN DEBIAN_FRONTEND=noninteractive JOBS=MAX npm install --unsafe-perm
+			this.hasScripts = this.hasScripts || _(packageJson.scripts).pick('preinstall', 'install', 'postinstall').size() > 0
 
-			COPY . ./
-			CMD ["npm", "start"]
-		`
+			let dockerfile: string
+			if (this.hasScripts) {
+				dockerfile = `
+					FROM resin/${bundle.deviceType}-node
+					WORKDIR /usr/src/app
+					RUN ln -s /usr/src/app /app
+					COPY . /usr/src/app
+					RUN DEBIAN_FRONTEND=noninteractive JOBS=MAX npm install --unsafe-perm
+					CMD [ "npm", "start" ]
+					`
+			} else {
+				dockerfile = `
+					FROM resin/${bundle.deviceType}-node
+					WORKDIR /usr/src/app
+					RUN ln -s /usr/src/app /app
 
-		const file: FileInfo = {
-			name: 'Dockerfile',
-			size: dockerfile.length,
-			contents: new Buffer(dockerfile)
-		}
+					COPY package.json .
+					RUN DEBIAN_FRONTEND=noninteractive JOBS=MAX npm install --unsafe-perm
 
-		return Promise.resolve([file])
+					COPY . ./
+					CMD ["npm", "start"]
+				`
+			}
+			const file: FileInfo = {
+				name: 'Dockerfile',
+				size: dockerfile.length,
+				contents: new Buffer(dockerfile)
+			}
+			return [ file ]
+		})
 	}
 }
